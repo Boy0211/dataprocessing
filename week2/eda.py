ca04b7950ccba33922e558e284cc426f04d993d5
Boy0211 <s.kuilboer@gmail.com>
2018-11-10 16:08:02 +0100

    Alleen de boxplot moet nog gecontroleerd worden

--- week2/eda.py
+++ week2/eda.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import json
+from statistics import mode, median, mean, stdev
 import matplotlib.pyplot as plt
 from matplotlib import style
 
@@ -8,8 +10,10 @@ class EDA():
     def __init__(self):
         df = self.load_data()
         df = self.processing_data(df)
+        # self.calculate_central_tendency_GDP(df)
+        # self.calculate_fivenumber(df)
+        self.write_json_file(df)
 
-        print(df)
 
     def load_data(self):
 
@@ -25,8 +29,71 @@ class EDA():
 
         df = df[df != 'unknown']
         df = df.dropna()
+        df = df.drop([193])
 
         return df
+
+    def calculate_central_tendency_GDP(self, df):
+
+        gdp = df['GDP ($ per capita) dollars'].tolist()
+
+        new_gdp = []
+        for country in gdp:
+            country = int(country.replace(' dollars', ''))
+            new_gdp.append(country)
+
+        print(f"mode: {mode(new_gdp)}")
+        print(f"median: {median(new_gdp)}")
+        print(f"mean: {mean(new_gdp)}")
+        print(f"std.dev: {stdev(new_gdp)}")
+
+        x = df['Country'].tolist()
+        y = new_gdp
+
+        plt.bar(x,y,align='center') # A bar chart
+        plt.xlabel('Countrys')
+        plt.ylabel('GDP in dollars')
+        plt.xticks(rotation=90, fontsize=5)
+        plt.rc('font', size=1)
+        plt.show(block=True)
+
+    def calculate_fivenumber(self, df):
+
+        statistic_data = (df['Infant mortality (per 1000 births)'].tolist())
+        statistic_data_new = []
+        for stat in statistic_data:
+            stat_new = stat.replace(',', '.')
+            stat_new = float(stat_new)
+            statistic_data_new.append(stat_new)
+
+        minimum = min(statistic_data_new)   # minimum
+        first_quartile = (np.percentile(statistic_data_new, 25))  # Q1
+        mediaan = median(statistic_data_new) # median
+        third_quartile = (np.percentile(statistic_data_new, 75))  # Q3
+        maximum = max(statistic_data_new)
+
+        five_summary = {"minimum": minimum,
+                        "first_quartile": first_quartile,
+                        "median": mediaan,
+                        "third_quartile": third_quartile,
+                        "maximum": maximum}
+        print(five_summary)
+
+
+        plt.xlabel("Infant mortality per 1000 births")
+        plt.title("Infant mortality")
+        plt.boxplot(statistic_data_new, patch_artist=True, vert=False)
+        plt.show()
+
+    def write_json_file(self, df):
+
+        df['Region'] = df['Region'].str.strip()
+        df1 = df.set_index('Country')
+        print(df1)
+
+        json = df1.to_json("CountryData.json", orient='index')
+        print(type(json))
+
 
 if __name__ == "__main__":
     eda = EDA()
